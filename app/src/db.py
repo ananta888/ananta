c25f8f866e69bd7333be57a65a7b50107848354e
Peter Stuiber <stuiber@ananta.de>
2025-08-08 01:45:52 +0200

    chore: remove unused database connection setup logic from db.py

--- app/src/db.py
+++
@@ -1,9 +0,0 @@
-import os
-import json
-import psycopg2
-from psycopg2.extras import Json
-
-DATABASE_URL = os.environ.get("DATABASE_URL", "postgresql://postgres:postgres@db:5432/ananta")
-
-
-def get_conn(retries=3, delay=1.0):
