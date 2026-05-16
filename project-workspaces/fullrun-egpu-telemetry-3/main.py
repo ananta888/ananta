bb188bab96a36858ef164d8cb7d90342ea720ecd
Peter Stuiber <stuiber@ananta.de>
2026-05-16 23:36:11 +0200

    fixup planning

---
+++ project-workspaces/fullrun-egpu-telemetry-3/main.py
@@ -0,0 +1,25 @@
+def main():
+    print("Hello from Anforderungen definieren!")
+
+if __name__ == "__main__":
+    main()
+def main():
+    print("Hello from Anforderungen definieren!")
+
+if __name__ == "__main__":
+    main()
+def main():
+    print("Hello from Anforderungen definieren!")
+
+if __name__ == "__main__":
+    main()
+def main():
+    print("Hello from Anforderungen definieren!")
+
+if __name__ == "__main__":
+    main()
+def main():
+    print("Hello from Anforderungen definieren!")
+
+if __name__ == "__main__":
+    main()
